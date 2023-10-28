46401fe502cdb9ab20341a543b62aa594524d11c
Murali Krishna <krishna.murali.ext@lastminute.com>
2023-10-28 22:08:29 +0530

    Initial Setup

---
+++ src/main/java/com/example/securityissue/SecurityIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityIssueApplication.class, args);
+    }
+
+}
